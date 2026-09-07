3346d1d626e9455d0a88bbf90e460f1f37be4c6c
Joseph Patton-Robson <10753344+webdev-kiwi@users.noreply.github.com>
2026-09-07 15:50:54 +1200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace KoboReader;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private async void MainWindow_Loaded(
+        object sender,
+        RoutedEventArgs e)
+    {
+
+        var env = await CoreWebView2Environment.CreateAsync(
+            null,
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "KoboReader"
+            )
+         );
+
+        await Browser.EnsureCoreWebView2Async(env);
+
+        Browser.CoreWebView2.NewWindowRequested += Browser_NewWindowRequested;
+
+        Browser.Source =
+            new Uri("https://read.kobobooks.com/");
+    }
+
+    private void Browser_NewWindowRequested(
+    object? sender,
+    CoreWebView2NewWindowRequestedEventArgs e)
+    {
+        e.Handled = true;
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = e.Uri,
+            UseShellExecute = true
+        });
+    }
+}
